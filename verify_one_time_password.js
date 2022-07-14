a290def2e48f4639359e69d5e82182b90d0cb54c
Carlos Craig <carlos.craig09@gmail.com>
2022-07-13 19:39:43 -0700

    ignores updated

--- verify_one_time_password.js
+++ verify_one_time_password.js
@@ -3,7 +3,7 @@ const { ref } = require("firebase-functions/v1/database");
 
 module.exports = function (req, res) {
   if (!req.body.phone || !req.body.code) {
-    return res.status(422).send({ error: "Phone and code must be provided" });
+    return res.status(422).send({ error: "Phone and code must be Provided" });
   }
 
   const phone = String(req.body.phone).replace(/[^\d]/g, "");
