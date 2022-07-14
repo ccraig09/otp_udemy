19c6801b096b779c95768bf135f5e9444f04a2ea
Carlos Craig <carlos.craig09@gmail.com>
2022-07-13 19:33:02 -0700

    changed auth token

--- twilio.js
+++ twilio.js
@@ -1,6 +1,6 @@
 const twilio = require("twilio");
 
 const accountSid = "AC768b267d328fc859e5fd5f7f718bb154";
-const authToken = "6fbb7dcc7b040e216092f739d16b8db3";
+const authToken = "cf312297feb062369f0a71690ba35700";
 
 module.exports = new twilio.Twilio(accountSid, authToken);
