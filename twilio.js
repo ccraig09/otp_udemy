ad2fba2e2b7f8d180f376971ba99bb111c458785
Carlos Craig <carlos.craig09@gmail.com>
2022-07-18 07:49:53 -0700

    Delete twilio.js

--- twilio.js
+++
@@ -1,6 +0,0 @@
-const twilio = require("twilio");
-
-const accountSid = "AC768b267d328fc859e5fd5f7f718bb154";
-const authToken = "cf312297feb062369f0a71690ba35700";
-
-module.exports = new twilio.Twilio(accountSid, authToken);
